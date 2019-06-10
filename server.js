205fa9b2f7d94669b4b943c562938ffbdfaa84b2
yashodhan14321 <yashodhan171907.cse@chitkara.edu.in>
2019-06-11 03:53:54 +0530

    first commit

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ var express = require('express')
 var app = express()
 var port = 3000
 
-app.listen(port , ()=>{console.log(`Listening on Port ${port}`)})
+
+//app can listen on any port mow running on port 3000
+app.listen(port , ()=>{console.log(`Listening on Port ${port} ....`)})
